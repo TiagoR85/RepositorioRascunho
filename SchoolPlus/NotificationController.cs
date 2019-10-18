12d8b98061053b5fc7cc036cad1e63989dd8403c
Tiago Rodrigues <Tiago.rds85@hotmail.com>
2019-10-18 14:54:20 -0300

    Dados para Hackathon

---
+++ SchoolPlus/NotificationController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using ProPlus.ApiWeb.SchoolPlus.Dominio.Models;
+
+namespace ProPlus.APIWeb.SchoolPlus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        [Produces("application/json")]
+        [HttpPost("create")]
+        public IActionResult CreateNotification(OneSignal pOptions)
+        {
+            var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+            request.KeepAlive = true;
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+
+            var serializer = new JavaScriptSerializer();
+            var obj = new
+            {
+                app_id = pOptions.ApplicationId,
+                headings = new { en = pOptions.Titulo },
+                contents = new { en = pOptions.Contexto },
+                include_player_ids = new string[] { string.Join(",", pOptions.IdsNotifications) }
+            };
+
+
+
+            var param = serializer.Serialize(obj);
+            byte[] byteArray = Encoding.UTF8.GetBytes(param);
+
+            string responseContent = null;
+
+            try
+            {
+                using (var writer = request.GetRequestStream())
+                {
+                    writer.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseContent = reader.ReadToEnd();
+                    }
+                }
+                return Ok(pOptions);
+            }
+            catch (WebException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                System.Diagnostics.Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+            }
+            System.Diagnostics.Debug.WriteLine(responseContent);
+            return Problem();
+
+        }
+    }
+}
---
+++ SchoolPlus/OneSignal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProPlus.ApiWeb.SchoolPlus.Dominio.Models
+{
+    public class OneSignal
+    {
+        public string ApplicationId { get; set; }
+        public string Titulo { get; set; }
+        public string Contexto { get; set; }
+        public List<string> IdsNotifications { get; set; }
+    }
+}
